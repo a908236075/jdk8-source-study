f0d98ca5079443ac136b27d394943ca23639e2f0
lbt <liubotao@yonyou.com>
2024-02-16 22:28:26 +0800

    JDK8-SRC源码

---
+++ src/test/java/FirstLearn.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class FirstLearn {
+    public static void main(String[] args) {
+        HashMap<String, String> h = new HashMap<>();
+        h.put("1", "1");
+    }
+}
